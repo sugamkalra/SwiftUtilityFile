6f86dd3075ae37df05dfc5f9a1eab3fccaae9c61
Sugam Kalra <sugamkalra@sugams-macbook-pro.local>
2016-02-08 17:14:22 +0530

    Initial Commit

---
+++ SwiftyUtilityFile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftyUtilityFile
+//
+//  Created by Sugam Kalra on 08/02/16.
+//  Copyright © 2016 Sugam Kalra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
